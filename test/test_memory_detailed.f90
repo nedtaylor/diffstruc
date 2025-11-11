4c1bd7451c288f7ee9c55d768fe17a64e180014c
Ned Taylor <71959356+nedtaylor@users.noreply.github.com>
2025-11-11 16:26:33 +0000

    Reintroduce test 1

--- test/test_memory_detailed.f90
+++ test/test_memory_detailed.f90
@@ -21,39 +21,37 @@ program test_memory_detailed
   write(*,*) "Testing different cleanup strategies..."
   write(*,*) ""
 
-  ! ! Test 1: Assignment with reset_graph
-  ! write(*,*) "Test 1: Using f = expr with reset_graph()"
-  ! call x%allocate(array_shape=[1000, 100])
-  ! call y%allocate(array_shape=[1000, 100])
-  ! x%val = 1.0_real32
-  ! y%val = 2.0_real32
-  ! call x%set_requires_grad(.true.)
-  ! call y%set_requires_grad(.true.)
+  ! Test 1: Assignment with reset_graph
+  write(*,*) "Test 1: Using f = expr with reset_graph()"
+  call x%allocate(array_shape=[1000, 100])
+  call y%allocate(array_shape=[1000, 100])
+  x%val = 1.0_real32
+  y%val = 2.0_real32
+  call x%set_requires_grad(.true.)
+  call y%set_requires_grad(.true.)
 
-  ! do i = 1, n_iterations
-  !   write(*,*) "=== Iteration ", i, " ==="
-  !    ! This creates a pointer, assigns it to f, but the pointer is lost
-  !   write(*,*) "loc f before assignment", loc(f)
-  !    f = x**2 !+ y * x + exp(x * 0.01_real32)
-  !   write(*,*) "loc f after assignment", loc(f)
-  !   !  f%owns_left_operand = .true.
-  !   !  f%owns_right_operand = .true.
-  !   ! call f%grad_reverse(record_graph=.false., reset_graph=.true.)
-
-  !    if (mod(i, 10) == 0) then
-  !       write(*,'(A,I0)') "  Iteration ", i
-  !    end if
-  ! end do
-  ! call f%print_graph()
-  ! !write(*,*) "  Final gradient x:", x%grad%val(1,1)
-  ! write(*,*) f%owns_left_operand, f%owns_right_operand, f%owns_gradient
-  ! call f%nullify_graph()
-  ! write(*,*) f%owns_left_operand, f%owns_right_operand, f%owns_gradient
-  ! call x%deallocate()
-  ! call y%deallocate()
-  ! call f%deallocate()
-  ! write(*,*) "  Test 1 complete"
-  ! write(*,*) ""
+  do i = 1, n_iterations
+     ! This creates a pointer, assigns it to f, but the pointer is lost
+     temp => x**2 + y * x + exp(x * 0.01_real32)
+     call f%assign_and_deallocate_source(temp)
+     f%is_temporary = .false.
+     call f%grad_reverse(record_graph=.false., reset_graph=.true.)
+     call f%nullify_graph()
+     call f%deallocate()
+     if (mod(i, 10) == 0) then
+        write(*,'(A,I0)') "  Iteration ", i
+     end if
+  end do
+  call f%print_graph()
+  !write(*,*) "  Final gradient x:", x%grad%val(1,1)
+  write(*,*) f%owns_left_operand, f%owns_right_operand, f%owns_gradient
+  call f%nullify_graph()
+  write(*,*) f%owns_left_operand, f%owns_right_operand, f%owns_gradient
+  call x%deallocate()
+  call y%deallocate()
+  call f%deallocate()
+  write(*,*) "  Test 1 complete"
+  write(*,*) ""
 
   ! Test 2: Pointer assignment with explicit deallocation
   write(*,*) "Test 2: Using temp => expr with explicit deallocate"
